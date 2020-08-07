6c9ce3092b85a10d5fc25d1ebae620b9175a446b
hkong1 <64344936+hkong1@users.noreply.github.com>
2020-08-07 16:01:53 -0500

    Add files via upload

---
+++ MySuperBank/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MySuperBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("Kendra", 10000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
+
+            account.MakeWithdrawal(500, DateTime.Now, "Rent payment");
+            Console.WriteLine(account.Balance);
+            account.MakeDeposit(100, DateTime.Now, "Friend paid me back");
+            Console.WriteLine(account.Balance);
+
+            // Test for a negative balance.
+            try
+            {
+                account.MakeWithdrawal(750, DateTime.Now, "Attempt to overdraw");
+            }
+            catch (InvalidOperationException e)
+            {
+                Console.WriteLine("Exception caught trying to overdraw");
+                Console.WriteLine(e.ToString());
+
+            }
+
+            Console.WriteLine(account.GetAccountHistory());
+        }
+    }
+}
